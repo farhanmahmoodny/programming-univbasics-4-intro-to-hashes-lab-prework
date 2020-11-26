d4d122db4e65034a580e38e9c4ed9e4680050a83
Farhan Mahmood <farhanmahmoodny@gmail.com>
2020-11-26 01:03:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,7 @@ end
 def my_hash_creator(key, value)
   # return a hash that includes the key and value par
   ameters passed into this method
-  hash = {"#{key}": value}
+  hash = {key: value}
   hash
 end
 
